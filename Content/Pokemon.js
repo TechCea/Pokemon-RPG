2c358139644eb30a8933d5657c549122f9eaee65
BJaviersito <losthyghost@gmail.com>
2024-08-27 15:25:21 -0600

    link

--- Content/Pokemon.js
+++ Content/Pokemon.js
@@ -10,25 +10,25 @@ window.PizzaTypes = {
     "s001": {
       name: "Slice Samurai",
       type: PizzaTypes.spicy,
-      src: "/IMG/Pokemons/Gengar.gif",
-      icon: "/IMG/icons/Ghost.png",
+      src: "IMG/Pokemons/Gengar.gif",
+      icon: "IMG/icons/Ghost.png",
     },
     "v001": {
       name: "Call Me Kale",
       type: PizzaTypes.veggie,
-      src: "/IMG/characters/pizzas/v001.png",
-      icon: "/IMG/icons/veggie.png",
+      src: "IMG/characters/pizzas/v001.png",
+      icon: "IMG/icons/veggie.png",
     },
     "f001": {
       name: "Portobello Express",
       type: PizzaTypes.fungi,
-      src: "/IMG/characters/pizzas/f001.png",
-      icon: "/IMG/icons/fungi.png",
+      src: "IMG/characters/pizzas/f001.png",
+      icon: "IMG/icons/fungi.png",
     },
     "P001": {
       name: "Gastly",
       type: PizzaTypes.fungi,
-      src: "/IMG/Pokemons/Datiny.gif",
-      icon: "/IMG/icons/fungi.png",
+      src: "IMG/Pokemons/Datiny.gif",
+      icon: "IMG/icons/fungi.png",
     }
   }
