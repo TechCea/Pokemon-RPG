af2bb1b83234554821240ea32d1313943a46e52e
BJaviersito <losthyghost@gmail.com>
2024-09-06 20:45:50 -0600

    hero

--- Game.Object.js
+++ Game.Object.js
@@ -7,7 +7,7 @@ class GameObject{
         this.direction = config.direction || "down";
         this.sprite = new Sprite({
             gameObject:this,
-            src: config.src || "IMG/characters/people/Baggins.png",
+            src: config.src || "IMG/characters/people/hero.png",
         });
 
         this.behaviorLoop = config.behaviorLoop || [];
